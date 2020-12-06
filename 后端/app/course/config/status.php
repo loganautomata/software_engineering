5125d3eff77d84de430cf2e54f9c32d89c3221f3
loganautomata <loganautomata@gmail.com>
2020-12-06 16:15:22 +0800

    删去后端仓库

--- 后端/app/course/config/status.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'login_successed' => 10, //登录成功
-    'login_failed' => 11, //登录失败
-    'login_exception_ip' => 12, //登录地址变更异常
-];
--- 后端/config/time.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'token' => 20 * 60,
-    'redis_token' => 20 * 60,
-    'refresh_token' => 24 * 60 * 60,
-    'leeway' => 3,
-    'nbf' => 3
-];
